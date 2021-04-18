20dcbd0d480d1cbccf2b95b11ab06f9bfba70b69
Dorneanu Eduard-Gabriel <eduard@snkrsden.com>
2021-04-18 21:28:14 +0300

    started drawer navigation

--- app/src/main/java/com/eduard/shoppinglist/MainActivity.java
+++ app/src/main/java/com/eduard/shoppinglist/MainActivity.java
@@ -1,84 +1,38 @@
 package com.eduard.shoppinglist;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView info;
-    private LoginButton facebookLogin;
-    private Button loginButton;
-    private EditText usernameInput;
-    private SharedPreferences sharedPreferences;
-    private SharedPreferences.Editor sharedPreferencesEditor;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.drawer_layout);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        sharedPreferences = this.getSharedPreferences(getString(R.string.login_shared_pref), Context.MODE_PRIVATE);
-        sharedPreferencesEditor = sharedPreferences.edit();
+        drawer = findViewById(R.id.drawer_layout);
 
-        usernameInput = findViewById(R.id.username_input);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
 
-        // Facebook login
-        facebookLogin = findViewById(R.id.facebook_login);
-
-        CallbackManager callbackManager = CallbackManager.Factory.create();
-
-        facebookLogin.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                sharedPreferencesEditor.putBoolean(getString(R.string.login_state), true);
-                sharedPreferencesEditor.putString(getString(R.string.login_username), loginResult.getAccessToken().getApplicationId());
-                sharedPreferencesEditor.apply();
-            }
-
-            @Override
-            public void onCancel() {
-
-            }
-
-            @Override
-            public void onError(FacebookException error) {
-
-            }
-        });
-
-
-        // Normal login
-        loginButton = findViewById(R.id.login_button);
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sharedPreferencesEditor.putBoolean(getString(R.string.login_state), true);
-                sharedPreferencesEditor.putString(getString(R.string.login_username), usernameInput.getText().toString());
-                sharedPreferencesEditor.apply();
-            }
-        });
-
-        // Check if user is logged in
-        if(sharedPreferences.getBoolean(getString(R.string.login_state), false)) {
-            info = findViewById(R.id.login_screen_enter_username);
-            info.setText(sharedPreferences.getString(getString(R.string.login_username), ""));
-            setContentView(R.layout.shopping_list_item_row);
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
 }
